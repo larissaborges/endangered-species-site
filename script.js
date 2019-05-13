4761cff9df545dbbcbf1a42697bddb0b2515dfb3
borges <larissafurtado12@live.com>
2019-05-13 15:06:59 -0300

    first-version

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log(window.location.href);
+
+const seletorPrimeiro = document.querySelectorAll('.ativo');
+console.log(seletorPrimeiro);
+console.log(seletorPrimeiro.innerText);
+
+const linguagemPadrao = window.navigator.language;
+console.log(linguagemPadrao);
+
+const larguraJanela = window.innerWidth;
+console.log(larguraJanela);
