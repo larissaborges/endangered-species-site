53bd5fd4d8d42b5e2059f37e940bec52cb028133
borges <larissafurtado12@live.com>
2019-05-21 17:09:16 -0300

    scroll-suave

---
+++ script.js
@@ -0,0 +1,21 @@
+function InitScrollSuave() {
+
+    const LinksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    }
+
+
+    LinksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection);
+
+    });
+}
